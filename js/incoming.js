953cd22beb1a06743f5050b7f2eb840a92a16978
sreylet <sreylet.sao@student.passerellesnumeriqes.org>
2025-01-23 11:47:43 +0700

    update incoming and outgoing

--- js/incoming.js
+++ js/incoming.js
@@ -157,7 +157,18 @@ document.addEventListener("DOMContentLoaded", () => {
     loadFolders();
 });
 
+function navbar() {
+    const menuIcon = document.querySelector("#menu");
+    const sideBar = document.getElementById("sideBar");
+    menuIcon.addEventListener("click", () => {
+        sideBar.classList.toggle("hidden");
+    });
+}
+navbar();
 
+const btnCreate = document.getElementById("btn");
+const cardDiragetorys = document.querySelector(".card-diragetorys");
+let currentFolder = null;
 
 
 
