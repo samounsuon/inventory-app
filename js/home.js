4de35f35667bd7340f0c1a5d8ee56591f672047b
Kartrork <kartrork1551@gmail.com>
2025-01-24 19:48:20 +0700

    correction nav

--- js/home.js
+++ js/home.js
@@ -1,8 +1,13 @@
-function navbar() {
-    const menuIcon = document.querySelector("#menu");
-    const sideBar = document.querySelector(".aside-bar");
-    menuIcon.addEventListener("click", () => {
-      sideBar.classList.toggle("hidden");
-    });
-  }
-  navbar();
+
+  // Get the menu button and sidebar
+const menuButton = document.getElementById("menu-button");
+const sidebar = document.getElementById("sidebar");
+const mainContent = document.getElementById("main-content");
+
+// Add event listener to the menu button
+menuButton.addEventListener("click", () => {
+  // Todo:classList.toggle the 'hidden' class for the sidebar
+  sidebar.classList.toggle("hidden");
+  // Todo:classList.toggle the 'compact' class for the main content
+  mainContent.classList.toggle("compact")
+});
